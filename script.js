8bd93ab3b67afcdd16f34e423ecc98425e64d92a
Evve mejia <119977867+evemeji@users.noreply.github.com>
2023-10-12 16:37:57 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+function encriptar() {
+    var texto = document.getElementById("input-texto").value.toLowerCase();
+    var txtCifrado = texto.replace(/e/igm,"enter");
+    var txtCifrado = txtCifrado.replace(/o/igm,"ober");
+    var txtCifrado = txtCifrado.replace(/i/igm,"imes");
+    var txtCifrado = txtCifrado.replace(/a/igm,"ai");
+    var txtCifrado = txtCifrado.replace(/u/igm,"ufat");
+
+
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+    alert("Se encriptó");
+
+}
+
+function desencriptar() {
+    var texto = document.getElementById("input-texto").value.toLowerCase();
+    var txtCifrado = texto.replace(/enter/igm,"e");
+    var txtCifrado = txtCifrado.replace(/ober/igm,"o");
+    var txtCifrado = txtCifrado.replace(/imes/igm,"i");
+    var txtCifrado = txtCifrado.replace(/ai/igm,"a");
+    var txtCifrado = txtCifrado.replace(/ufat/igm,"u");
+
+
+    document.getElementById("muneco").style.display = "none";
+    document.getElementById("texto").style.display = "none";
+    document.getElementById("texto2").innerHTML = txtCifrado;
+    document.getElementById("copiar").style.display = "show";
+    document.getElementById("copiar").style.display = "inherit";
+    alert("Se desencriptó");
+
+}
+
+function copy() {
+    var contenido = document.querySelector("#texto2");
+    contenido.select();
+    document.execCommand("copy");
+    alert("Se copio");
+}
